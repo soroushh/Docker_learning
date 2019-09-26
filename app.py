d26f314618f652af69bf860e576e38a49a2ee983
soroush <soroush@tails.com>
2019-09-26 16:53:27 +0100

    We are able to use the redis now.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import redis
 from flask import Flask
+from flask import jsonify
+
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -10,13 +12,25 @@ def set_key_value(key, value):
 
 
 def get_value(key):
-    cache.get(key)
+    return cache.get(key)
 
 
+@app.route('/<name>/<family>')
+def hello(name, family):
+    set_key_value(name, family)
+    return jsonify(
+        {
+            "message": "It was successfully loaded to redis instance"
+        }
+    )
 
+@app.route('/<name>')
+def say_family(name):
+    if get_value(name):
+        return get_value(name)
+    else:
+        return jsonify({
+            "message": "The family does not exist."
+        })
 
-@app.route('/<first>/<second>/<third>')
-def hello(first, second, third):
-    set_key_value(first, second)
-    return cache.get(third)
 
